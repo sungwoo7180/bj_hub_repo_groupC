95f22d4479aad24bbbd072cd60ef4445d4e8feda
sungwoo7180 <128414393+sungwoo7180@users.noreply.github.com>
2024-10-22 18:11:44 +0900

    [Silver V] Title: 줄세우기, Time: 396 ms, Memory: 24056 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,44 +1,60 @@
+import java.util.Arrays;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        // 테스트 케이스의 수 입력
-        int testCases = scanner.nextInt();
+        // 20크기의 int 형 배열 선언 or 21 크기
+        // 2차원 배열로 해야겠네 input 다 받고 출력
+        // 1. 열 숫자로 받고 그만큼 for 문
+        // 2. String 으로 한 줄 받음 -> split 으로 나누고 나눈 값에 Int 형으로 형 변환 해서 배열에 넣기
+        //
+        int a = scanner.nextInt();
+        scanner.nextLine();     // 버퍼
+        int[][] lineUpArr = new int[a][20];
+        String[] inputData = new String[a];
 
-        // 각 테스트 케이스 처리
-        for (int t = 0; t < testCases; t++) {
-            int testCaseNumber = scanner.nextInt();  // 테스트 케이스 번호 입력
-
-            int[] heights = new int[20];  // 20명의 학생 키를 저장할 배열
-            for (int i = 0; i < 20; i++) {
-                heights[i] = scanner.nextInt();  // 학생들의 키 입력
+        for ( int i = 0 ; i < a ; i++ ) {
+            inputData[i] = scanner.nextLine();
+            String[] lineUp = inputData[i].split(" ");
+            lineUp = Arrays.copyOfRange(lineUp, 1, lineUp.length);  // 첫번째 index 값을 제외한 나머지 값들을 저장
+            for ( int num = 0 ; num < 20 ; num++ ) {
+                lineUpArr[i][num] = parseInt(lineUp[num]);
             }
-
-            int moveCount = 0;  // 학생들이 이동한 총 횟수
-
-            // 삽입 정렬을 사용하여 이동 횟수 계산
-            for (int i = 1; i < 20; i++) {
-                int current = heights[i];  // 현재 학생의 키
-                int j = i - 1;
-
-                // 현재 학생보다 키가 큰 학생들을 뒤로 이동
-                while (j >= 0 && heights[j] > current) {
-                    heights[j + 1] = heights[j];  // 한 칸씩 뒤로 이동
-                    j--;
-                    moveCount++;  // 이동 횟수 증가
-                }
-
-                // 현재 학생을 올바른 위치에 삽입
-                heights[j + 1] = current;
-            }
-
-            // 결과 출력 (테스트 케이스 번호와 이동 횟수)
-            System.out.println(testCaseNumber + " " + moveCount);
         }
 
-        scanner.close();  // Scanner 닫기
+        // solve1) 주어진대로 무식하게 배열에서 한칸씩 옮겨주기
+        for ( int i = 0 ; i < a ; i++ ) {
+            int moveCount = 0;          // 학생들이 이동한 수
+            int[] countArray = new int[20];
+
+            for ( int num = 0 ; num < 20 ; num++ ) {    // num = 몇번째 학생
+                int position = 0;           // 기존에 학생들 비교 index
+                countArray[num] = lineUpArr[i][num];
+                // 삽입한 후에 비교해주는 연산
+                while ( position < num  && countArray[position] < countArray[num]) {
+                    position++;
+                }
+                moveCount += num - position;
+
+                if ( position != num ) {
+                    int temp = countArray[num];
+
+                    // 위치부터 num 까지 한 칸씩 뒤로 밀기
+                    for (int j = num; j > position; j--) {
+                        countArray[j] = countArray[j - 1];
+                    }
+
+                    // 삽입할 위치에 학생 삽입
+                    countArray[position] = temp;
+                }
+            }
+            System.out.println(i+1 + " " + moveCount);
+        }
     }
 }
